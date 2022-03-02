643d791a85dcba24ed05ad5bd892d40b2cfb87ee
jaymish <jaymishptl@gmail.com>
2022-03-02 15:45:18 -0800

    docker

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:16-jdk-alpine
+
+COPY . /usr/src/app/
+WORKDIR /usr/src/app/
+RUN ./gradlew clean build -x test
+RUN mv build/libs/producerOrderKafka-0.0.1-SNAPSHOT.jar producerOrderKafka.jar
+
+ENTRYPOINT java -jar producerOrderKafka.jar
--- src/main/java/com/order/orderkafka/service/kafka/ProducerService.java
+++ src/main/java/com/order/orderkafka/service/kafka/ProducerService.java
@@ -16,30 +16,19 @@ import java.util.Random;
 @Slf4j
 public class ProducerService {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+
     private final KafkaTemplate<String, Orders> weatherKafkaTemplate;
 
-    @Value("${kafka.topic.string-demo.name}")
-    private String STRING_TOPIC;
 
     @Value("${kafka.topic.json.name}")
     private String JSON_TOPIC;
 
-    public ProducerService(KafkaTemplate<String, String> kafkaTemplate, KafkaTemplate<String, Orders> weatherKafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    public ProducerService(KafkaTemplate<String, Orders> weatherKafkaTemplate) {
+
         this.weatherKafkaTemplate = weatherKafkaTemplate;
     }
 
-    public void sendMessage(String message){
-        log.info(String.format("$$$$ => Consumed Message: %s", message));
-        List<String> keyList = Arrays.asList("KEY1","KEY2","KEY3","KEY4","KEY5");
 
-        kafkaTemplate.executeInTransaction(t -> {
-             t.send(STRING_TOPIC, keyList.get(new Random().nextInt(keyList.size())), message);
-
-            return true;
-        });
-    }
 
     public void sendMessageJson(Orders orders){
         log.info(String.format("$$$$ => Consumed Message: %s", orders));
